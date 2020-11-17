c0e8524c32748dbe4a323b573320e855909b8981
Simone Ungaro <simoneungaro@hotmail.it>
2020-11-17 23:40:42 +0100

    Add tailwindcss

--- src/render/main.ts
+++ src/render/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/styles/tailwind.css';
+
 import router from './router'
 
 const electron = require('electron')
--- src/render/views/Home.vue
+++ src/render/views/Home.vue
@@ -6,19 +6,30 @@
   </div>
   <p>count: {{state.count}}</p>
   <p>double: {{state.double}}</p>
+
+  <div class="bg-white rounded-lg p-6">
+       <img class="h-16 w-16 rounded-full mx-auto">
+    <div class="text-center">
+         <h2 class="text-lg">Erin Lindford</h2>
+         <div class="text-purple-500">Product Engineer</div>
+         <div class="text-gray-600">erinlindford@example.com</div>
+         <div class="text-gray-600">(555) 765-4321</div>
+       </div>
+     </div>
 </template>
 
-<script>
-import Helo from '../components/HelloWorld.vue'
+<script lang='ts'>
+import Hello from '../components/HelloWorld.vue'
 import { reactive, computed } from 'vue'
 const version = require('process').versions.electron
+
 export default {
   name: 'Index',
   components: {
-    Helo
+    Hello
   },
   setup() {
-    const state = reactive({
+    const state:any = reactive({
       count: 0,
       double: computed(() => state.count * 2),
       version: version
@@ -35,5 +46,3 @@ export default {
   },
 }
 </script>
-</style>
-
